e6c791d25d7d604d00dcc7877160fdd80c530b75
Isaac Addis <isaacaddis21@gmail.com>
2018-03-31 05:11:44 -0700

    Requests

--- vision/app.js
+++ vision/app.js
@@ -5,7 +5,7 @@ var util = require('util');
 var mime = require('mime');
 var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
-
+var request = require('request');
 var config = {
   projectId: 'lahackhack-199707',
   keyFilename: 'LAHack-594fb78d43e9.json'
@@ -21,6 +21,7 @@ var form = '<!DOCTYPE HTML><html><body>' +
   "<input type='file' name='image'/>" +
   "<input type='submit' /></form>" +
   '</body></html>';
+
 function base64Image(src) {
   var data = fs.readFileSync(src).toString('base64');
   return util.format('data:%s;base64,%s', mime.lookup(src), data);
@@ -29,7 +30,41 @@ app.get('/', function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  res.end(form);
+  res.end("Hey! Did you mean to issue a specific request?");
+});
+app.get('/img', function(req, res){
+  var take_after = req.query.take_after;
+  res.end(take_after);
+  request(' ', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      /*
+        FORMAT:
+        {
+            id: string;
+            img: string;
+            createdAt: number;
+        }
+      */
+      var id = response.id;
+      var img = base64Image(response.img);
+      var createdAt = response.createdAt;
+        client
+          .labelDetection(img)
+          .then(results => {
+            res.write('<!DOCTYPE HTML><html><body>');
+
+            // Base64 the image so we can display it on the page
+          res.write('<img width=200 src="' + img + '"><br>');
+            const labels = results[0].labelAnnotations;
+            console.log('Labels:');
+            labels.forEach(label => console.log(JSON.stringify(label, null, 4)));
+            res.end('</body></html>');
+          })
+          .catch(err => {
+            console.error('ERROR:', err);
+          });
+    }
+  });
 });
 
 
